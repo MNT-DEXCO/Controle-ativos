3ee26a4a4530b7cede44de972a2e1af44ffb0141
paulinhoal00-design <paulinhoal00@gmail.com>
2026-09-21 07:00:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dexco-ativos-v5';
+const CACHE_NAME = 'dexco-ativos-v6';
 const CORE_ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png'];
 
 self.addEventListener('install', event => {
